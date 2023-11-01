2cc16b3762a78cfb9adb3d46878c7630b4355cea
michael-lim <93946005+michael-lim@users.noreply.github.com>
2023-11-01 14:43:15 +0100

    자바스크립트파일 생성

--- app/index.js
+++ app/index.js
@@ -12,6 +12,7 @@ const home = require("./src/routes/home");
 //앱셋팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); //미들웨어를 구동시켜주는 메서드
 
